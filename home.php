2a28ae6f650d5396d8201effb88983dd0f65c336
Newzanman <panuwat.panyawai@gmail.com>
2015-04-07 15:40:58 +0700

    Sciequipment

---
+++ home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>	
+  
+    <meta charset="utf-8">	
+	<script  type="text/javascript" src="js/jquery.js"></script>
+	 <meta http-equiv="content-Type" content="text/html; charset=tis-620"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>index</title>			
+	<script  type="text/javascript" src="js/bootstrap.min.js."/></script>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/css.css" type="text/css/">
+ 
+
+
+</head>
+
+<body>
+  
+ <?php
+ include "header.php";
+ ?>
+
+<div class="container"> 
+
+ <fieldset >
+ <div class="row">  
+<br>
+<br>
+<br>
+  
+
+   
+   <center><img src="pic/law.png" width="570" height="770"></center>
+   
+	
+
+    </div>
+ 
+    
+  
+</fieldset>
+
+<?php
+ include "footer.php";
+ ?>
+
+</body>
+</html>
+
